00c6c6bf3dd358a5734e9c508ab23d4aa339c07f
Anton Marchukov <anton@marchukov.com>
2019-02-06 14:36:04 +0100

    Fixed bug url parsing to force "bug-url:" keyword.
    Expected implementation should extract bugzilla link from
    the bug-url field only. Current implementation scrubs
    commit message for all links to bugzilla. This fails when
    bug is just mentioned instead of being declared as fixed.
    
    This commit changes bug extraction to only look for bug
    urls if the following conditions are met:
    
    1. It starts with "Bug-Url" keyword followed by ":".
    2. "Bug-Url" keyword is at the beginning of a new line.
    3. Spaces/tabs are ignored between ":" and bug url.
    4. Search is not case-sensitive.
    
    Fixes: https://ovirt-jira.atlassian.net/browse/OVIRT-2669
    Change-Id: If0f2eff5a0a95f42981eff3d8360b4a3085326dc

--- hooks/lib/bz.py
+++ hooks/lib/bz.py
@@ -185,7 +185,8 @@ class Bugzilla(object):
         :return: list of bug urls or empty list
         """
 
-        regex_search = r'https*:\/\/' + bz_server.split("//")[-1] + r'\/.*\d+\b'
+        regex_search = r'^Bug-Url:[ \t]*https*:\/\/' + \
+                       bz_server.split("//")[-1] + r'\/.*\d+\b'
         regex_flags = re.IGNORECASE
 
         return re.findall(regex_search, commit, regex_flags)
